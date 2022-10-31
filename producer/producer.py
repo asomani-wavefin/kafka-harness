dd5d0608dca09872d5325e33042c0879038eea13
Ayaz Somani (Wave) <asomani@waveapps.com>
2022-10-31 09:08:18 -0400

    Central config module added

--- producer/producer.py
+++ producer/producer.py
@@ -18,32 +18,33 @@ import logging
 import time
 
 # Local imports
+from .config import settings
 
 
-CLI_DESCRIPTION = "CLI for a simply Kafka producer."
-CLI_PROG = "producer"
-
-VERBOSE_LOG_LEVEL = logging.WARN
+# CLI_DESCRIPTION = settings.get('CLI_DESCRIPTION')
+# CLI_PROG = settings.get('CLI_PROG')
+# STANDARD_LOG_LEVEL = settings.get('STANDARD_LOG_LEVEL') 
+# VERBOSE_LOG_LEVEL = settings.get('VERBOSE_LOG_LEVEL')
 
 def main():
     """Main execution path. Parses the command line and delegates.
     """
 
     # Supress visibility of logger output
+    logging.basicConfig(level=settings.get('STANDARD_LOG_LEVEL'))
     logger = logging.getLogger()
-    logger.setLevel(logging.CRITICAL)
 
     # Set up argparse
-    parser = argparse.ArgumentParser(description=CLI_DESCRIPTION, prog=CLI_PROG)
+    parser = argparse.ArgumentParser(description=settings.get('CLI_DESCRIPTION'), prog=settings.get('CLI_PROG'))
     parser.add_argument('--verbose', action="store_true", help="make the output verbose")
     parser.add_argument('-v', '--version', action="version", help="print the version of this software",
-        version='{prog} version {ver}'.format(prog=CLI_PROG, ver=__version__))
+        version='{prog} version {ver}'.format(prog=settings.get('CLI_PROG'), ver=__version__))
 
     # Producer variables
-    parser.add_argument('--bootstrap', action="store", type=ascii, default="localhost:9092", help="comma-separated list for bootstrap.servers")
-    parser.add_argument('--count', action="store", type=int, default=10, help="number of messages to generate")
-    parser.add_argument('--sleep', action="store", type=int, default=3, help="interval between message sends in seconds")
-    parser.add_argument('--topic', action="store", type=ascii, default='wav-test', help="topic on which messages will be sent")
+    parser.add_argument('--bootstrap', action="store", type=ascii, default=settings.get('ARG_BOOTSTRAP'), help="comma-separated list for bootstrap.servers")
+    parser.add_argument('--count', action="store", type=int, default=settings.get('ARG_COUNT'), help="number of messages to generate")
+    parser.add_argument('--sleep', action="store", type=int, default=settings.get('ARG_SLEEP'), help="interval between message sends in seconds")
+    parser.add_argument('--topic', action="store", type=ascii, default=settings.get('ARG_TOPIC'), help="topic on which messages will be sent")
 
     # Parse the command line and call the command
     try:
@@ -51,15 +52,19 @@ def main():
 
         # verbose option increases the logger visibility
         if args.verbose:
-            logger.setLevel(VERBOSE_LOG_LEVEL)
-            print("verbose output mode active")
+            logger.setLevel(settings.get('VERBOSE_LOG_LEVEL'))
+            print("output mode set to VERBOSE\n")
         
-        bootstrap = args.bootstrap.strip("\'")
-        count = args.count
-        topic = args.topic.strip("\'")
-        sleep = args.sleep
+        push_messages(
+            args.bootstrap.strip("\'"), 
+            args.count, 
+            args.topic.strip("\'"), 
+            args.sleep, 
+            receipt_callback=cb_receipt)
 
-        push_messages(bootstrap, count, topic, sleep, receipt_callback=cb_receipt)
+    except:
+        print("An error occured")
+        exit(1)
 
     finally:
         print("")
@@ -77,7 +82,6 @@ def init_producer(bootstrap_servers='') -> Producer:
     try:
         p = Producer(
             {'bootstrap.servers': bootstrap_servers})
-            # {'bootstrap.servers':'localhost:9092'})
         if p is None:
             logger.critical('Failed to initiate Kafka Producer!')
             return None
@@ -96,7 +100,7 @@ def generate_message() -> str:
     # use Faker to generate dummy message data
     fake = Faker()
     if fake is None:
-        return None
+        return '{}'
 
     # data structure of the json message
     data = {
@@ -125,7 +129,7 @@ def cb_receipt(err, msg):
     if err is not None:
         logger.error('{}'.format(err))
     else:
-        message = 'Produced message. Topic: {}. Value {}\n'.format(
+        message = 'Produced message. Topic: {}. Value: {}\n'.format(
             msg.topic(), 
             msg.value().decode('utf-8'))
 
@@ -141,12 +145,12 @@ def push_messages(
     """Push a number of messages to a topic.
     """
 
-    print('bootstrap_servers: ' + bootstrap_servers)
-    print('message_count: ' + str(message_count))
-    print('topic: ' + topic)
-    print('sleep_time: ' + str(sleep_time))
-
     logger = logging.getLogger()
+
+    logger.info('bootstrap_servers: ' + bootstrap_servers)
+    logger.info('message_count: ' + str(message_count))
+    logger.info('topic: ' + topic)
+    logger.info('sleep_time: ' + str(sleep_time))
 
     # Get a Producer
     p = init_producer(bootstrap_servers=bootstrap_servers)
@@ -167,8 +171,8 @@ def push_messages(
             p.poll(1)
 
             # Asynchronously send the generated message to the Kafka topic
-            # topic = 'user-tracker'
             p.produce(topic, msg.encode('utf-8'), callback=receipt_callback)
+
         except KafkaException as e:
             logger.exception(e)
             p.flush()
